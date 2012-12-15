27dc29043bf0c6c1730cab20df6a6adb1c9e44bc
Maciek <vonproteus+github.com@gmail.com>
2012-12-16 00:15:29 +0100

    -y

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install git mpd mpc python-pyudev python-mpd -Y
+sudo apt-get install git mpd mpc python-pyudev python-mpd -y
 
 git clone git://github.com/vonProteus/RPi-OBABP.git
 
